eb5ff5c61f7913714c4c0d7921d8e8b2b28f48f1
Ayush Jha <ayush.jha@fusemachines.com>
2021-06-18 13:30:06 +0545

    added some logic to headers for cors

--- internal/response/headers.go
+++ internal/response/headers.go
@@ -3,8 +3,21 @@ package response
 import "net/http"
 
 func SetupHeaders(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	if acrh, ok := req.Header["Access-Control-Request-Headers"]; ok {
+		(*w).Header().Set("Access-Control-Allow-Headers", acrh[0])
+	}
+	(*w).Header().Set("Access-Control-Allow-Credentials", "True")
+	if acao, ok := req.Header["Access-Control-Allow-Origin"]; ok {
+		(*w).Header().Set("Access-Control-Allow-Origin", acao[0])
+	} else {
+		if _, oko := req.Header["Origin"]; oko {
+			(*w).Header().Set("Access-Control-Allow-Origin", req.Header["Origin"][0])
+		} else {
+			(*w).Header().Set("Access-Control-Allow-Origin", "*")
+		}
+	}
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+	(*w).Header().Set("Connection", "Close")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Messages, Suggestions, Conversation-Token")
 	(*w).Header().Set("Content-Type", "application/json")
 }

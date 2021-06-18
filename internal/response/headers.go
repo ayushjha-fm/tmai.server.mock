d3a85d267b433e3323cb78c4068d4b431f249c3a
Ayush Jha <ayush.jha@fusemachines.com>
2021-06-18 13:18:49 +0545

    add necesary headers to allowed headers

--- internal/response/headers.go
+++ internal/response/headers.go
@@ -5,5 +5,6 @@ import "net/http"
 func SetupHeaders(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Messages, Suggestions, Conversation-Token")
+	(*w).Header().Set("Content-Type", "application/json")
 }
